5a962db80b778f71495a9de0dc218ea3a75567e9
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2024-01-07 13:22:38 +0000

    Add healthcheck endpoint

--- tarefas-app-web-api/src/main/java/eu/jgabriel/tarefasapp/TarefasAppApiApplication.java
+++ tarefas-app-web-api/src/main/java/eu/jgabriel/tarefasapp/TarefasAppApiApplication.java
@@ -2,12 +2,20 @@ package eu.jgabriel.tarefasapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TarefasAppApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TarefasAppApiApplication.class, args);
+	}
+
+	@GetMapping("/healthcheck")
+	public String healthcheck(){
+		return "Ok";
 	}
 
 }
