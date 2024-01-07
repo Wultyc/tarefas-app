572fcf0ad5898484c6546b2e6b5b9875dabdf5f9
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2024-01-07 02:17:57 +0000

    Add scripts to create database, user and grant privileges

---
+++ tarefas-app-db/database_initial_setup.sql
@@ -0,0 +1,9 @@
+-- Create tarefas_app database
+CREATE DATABASE tarefas_app;
+
+-- Create tarefas_app_user user
+-- The string oPnAwt6G is placeholder for a real password. Please update the value before executing the script
+CREATE USER tarefas_app_user WITH ENCRYPTED PASSWORD 'oPnAwt6G';
+
+-- Grant permissions to tarefas_app_user user
+GRANT ALL PRIVILEGES ON DATABASE tarefas_app TO tarefas_app_user;
