a1136b67ebae2d3c0cd94800e461af50056ff362
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2024-01-07 21:56:13 +0000

    Replace JPA implementation with JPAStreamer

--- tarefas-app-web-api/src/main/java/eu/jgabriel/tarefasapp/users/UserService.java
+++ tarefas-app-web-api/src/main/java/eu/jgabriel/tarefasapp/users/UserService.java
@@ -1,20 +1,26 @@
 package eu.jgabriel.tarefasapp.users;
 
+import com.speedment.jpastreamer.application.JPAStreamer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
-    private final UserRepository userRepository;
+    private final JPAStreamer jpaStreamer;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserService(JPAStreamer jpaStreamer) {
+        this.jpaStreamer = jpaStreamer;
     }
 
     public List<User> getUsers(){
-        return userRepository.findAll();
+        List<User> allUsers = jpaStreamer.stream(User.class)
+                .sorted(User$.id)
+                .collect(Collectors.toList());
+
+        return allUsers;
     }
 }
