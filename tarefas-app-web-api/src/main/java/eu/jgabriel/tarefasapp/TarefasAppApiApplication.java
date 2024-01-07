8414d10301da7536fe5b2e25ed712ae37be143ed
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2024-01-07 12:20:57 +0000

    init Spring Boot Project

---
+++ tarefas-app-web-api/src/main/java/eu/jgabriel/tarefasapp/TarefasAppApiApplication.java
@@ -0,0 +1,13 @@
+package eu.jgabriel.tarefasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefasAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefasAppApiApplication.class, args);
+	}
+
+}
---
+++ tarefas-app-web-api/src/test/java/eu/jgabriel/tarefasapp/TarefasAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.jgabriel.tarefasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefasAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
