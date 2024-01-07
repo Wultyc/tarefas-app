c84c63ed8f418496b12f2b40c375815024762c8f
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2024-01-07 21:56:35 +0000

    Remove unneeded repositories interfaces

--- tarefas-app-web-api/src/main/java/eu/jgabriel/tarefasapp/roles/RolesRepository.java
+++
@@ -1,8 +0,0 @@
-package eu.jgabriel.tarefasapp.roles;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RolesRepository  extends JpaRepository<Role, Integer> {
-}
