da796d1110a04fc3dfb9db16f559614c1bcd9c01
J. Gabriel Azevedo <46544597+Wultyc@users.noreply.github.com>
2024-01-07 19:26:53 +0000

    Update database_initial_setup.sql

--- tarefas-app-db/database_initial_setup.sql
+++ tarefas-app-db/database_initial_setup.sql
@@ -7,3 +7,5 @@ CREATE USER tarefas_app_user WITH ENCRYPTED PASSWORD 'oPnAwt6G';
 
 -- Grant permissions to tarefas_app_user user
 GRANT ALL PRIVILEGES ON DATABASE tarefas_app TO tarefas_app_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO tarefas_app_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO tarefas_app_user;
